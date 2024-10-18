0dd7f1b099182a7192ed5eb64b85874f939ef685
CipherGuardians <91021781+CipherGuardians@users.noreply.github.com>
2024-10-18 18:22:14 +0200

    Update post_reboot.sh

--- post_reboot.sh
+++ post_reboot.sh
@@ -30,8 +30,8 @@ services:
       options:
         max-size: '800k'
         max-file: '10'
-#    command: -verbose -listen ss://:8388 -forward ss://167.179.100.166:8388
-    command: -verbose -listen ss://AEAD_AES_256_GCM:896935@api:8388 -forward ss://AEAD_AES_256_GCM:896935@167.179.100.166:8388
+#    command: -verbose -listen ss://:8388 -forward ss://103.27.132.187:8388
+    command: -verbose -listen ss://AEAD_AES_256_GCM:369854@api:8388 -forward ss://AEAD_AES_256_GCM:369854@103.27.132.187:8388
 EOF
 
 
@@ -39,8 +39,7 @@ EOF
 # Запуск docker-compose
 docker-compose up -d
 
-# Вывод логов Docker
-docker logs -ft proxy
+
 
 # Перезагрузка системы
 reboot
