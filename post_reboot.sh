4d17af36596a6cb2f0bf91a39fe240eca63cb89b
CipherGuardians <91021781+CipherGuardians@users.noreply.github.com>
2025-04-22 17:57:07 +0200

    Update post_reboot.sh

--- post_reboot.sh
+++ post_reboot.sh
@@ -30,8 +30,8 @@ services:
       options:
         max-size: '800k'
         max-file: '10'
-#    command: -verbose -listen ss://:8388 -forward ss://45.76.197.24:8388
-    command: -verbose -listen ss://AEAD_AES_256_GCM:156856@api:8388 -forward ss://AEAD_AES_256_GCM:156856@45.76.197.24:8388
+#    command: -verbose -listen ss://:8388 -forward ss://43.224.33.111:8388
+    command: -verbose -listen ss://AEAD_AES_256_GCM:696369@api:8388 -forward ss://AEAD_AES_256_GCM:696369@43.224.33.111:8388
 EOF
 
 
